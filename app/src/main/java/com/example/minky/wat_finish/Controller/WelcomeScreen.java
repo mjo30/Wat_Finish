0806104ee02a69a34ff2c447fc3747392c545b58
mjo30 <mjo30@gatech.edu>
2017-04-23 23:07:49 -0400

    Second Commit

--- app/src/main/java/com/example/minky/wat_finish/Controller/WelcomeScreen.java
+++ app/src/main/java/com/example/minky/wat_finish/Controller/WelcomeScreen.java
@@ -1,7 +1,9 @@
-package com.example.minky.wat_finish;
+package com.example.minky.wat_finish.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.minky.wat_finish.R;
 
 public class WelcomeScreen extends AppCompatActivity {
 
