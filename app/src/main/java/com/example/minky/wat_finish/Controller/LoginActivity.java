47cd7b05caeb1e5a69839ea95a549c882b0642c5
dmsqls0427k <danaeblee@gmail.com>
2017-04-24 00:11:06 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/minky/wat_finish/Controller/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.minky.wat_finish.Controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.minky.wat_finish.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
