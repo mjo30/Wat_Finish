c928e169ed5c85157f8dc359e3794ef132300611
mjo30 <mjo30@gatech.edu>
2017-04-24 00:39:54 -0400

    Fourth Commit

--- app/src/main/java/com/example/minky/wat_finish/Controller/LoginActivity.java
+++ app/src/main/java/com/example/minky/wat_finish/Controller/LoginActivity.java
@@ -1,14 +1,29 @@
 package com.example.minky.wat_finish.Controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.minky.wat_finish.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        registerBtn = (Button) findViewById(R.id.registerBtn);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Registration.class));
+            }
+        });
     }
 }
